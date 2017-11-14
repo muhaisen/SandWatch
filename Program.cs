b65f6360fbcc0355a3cd125682de6bb2eb226efe
BAF\abmu <abdallah.muhaisen@ikano.se>
2017-11-14 14:21:40 +0100

    intial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using AmazedSaint.Elastic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SandWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+               var cl = new WebClient();
+            Console.WriteLine("Reading public time line");
+            // To Read from URI      
+                //  / //_//
+               //  /_// \/         
+             //var cl = new WebClient();
+             //Console.WriteLine("Reading public time line");
+             //using (var r = new StreamReader
+             //    (cl.OpenRead(@"http://twitter.com/statuses/user_timeline/amazedsaint.xml")))
+             //{
+             //    var data = r.ReadToEnd();
+             //    IterateTweets(data);
+             //}
+             XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\DiamondWebService.xml");
+            //doc.Load(@"C:\AWS.xml");
+
+            string xmlcontents = doc.InnerXml;
+
+            var idk = doc.GetElementsByTagName("con:operation");
+            string returnTxt =   PrintAll(idk);
+            WriteItDown(returnTxt);
+            var tst =  doc.ChildNodes[1].ChildNodes[1].ChildNodes[3].ChildNodes[1];
+
+        }
+        static string PrintAll(XmlNodeList XmlList) {
+            string AllText = "";
+            foreach (XmlNode item in XmlList) {
+                var call = item["con:call"];
+
+               string Req = call["con:request"].InnerXml;
+                string endpoint = call["con:endpoint"].InnerXml;
+
+                int ends = Req.Length - 12;
+                System.Diagnostics.Debug.WriteLine(endpoint);
+
+                Req = Req.Substring(9, ends);
+                Req = Req.Replace("\\r", "\\r\\n");
+
+
+                AllText += endpoint + Environment.NewLine;
+                AllText += Req + Environment.NewLine;
+
+            }
+            return AllText;
+        }
+        static void WriteItDown(string TheText) {
+            string path = @"C:\Users\abmu\Desktop\lol.json";
+            if (!File.Exists(path))
+            { 
+                File.WriteAllText(path, TheText);
+            }
+             File.AppendAllText(path, TheText);
+        }
+    }
+}
